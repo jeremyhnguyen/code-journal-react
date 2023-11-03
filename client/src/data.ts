bb6b1cb740089933588c5d508283d1f5f86e458e
Jeremy Nguyen <jeremycnguyen@gmail.com>
2023-11-03 20:40:46 +0000

    added data.ts updates

--- client/src/data.ts
+++ client/src/data.ts
@@ -1,33 +1,60 @@
-
-export type Data = {
-  entries: [
-    {
-      title:string;
-      photoURL:string;
-      notes:string;
-      entryId:number;
-    },
-  ];
-  editing: null;
+export type UnsavedEntry = {
+  title: string;
+  notes: string;
+  photoUrl: string;
+};
+export type Entry = UnsavedEntry & {
+  entryId: number;
+};
+type Data = {
+  entries: Entry[];
   nextEntryId: number;
 };
-
-let data: Data = {
-  entries: [],
-  editing: null,
-  nextEntryId: 1,
-};
-
-window.addEventListener('beforeunload', function (event) {
-  const dataJSON = JSON.stringify(data);
-  localStorage.setItem('code-journal-data', dataJSON);
-});
-
-const localData = JSON.parse(localStorage.getItem('code-journal-data'));
-if (localData) {
-  data = localData;
-}
-
-export function getData() {
+const dataKey = 'code-journal-data';
+function readData(): Data {
+  let data: Data;
+  const localData = localStorage.getItem(dataKey);
+  if (localData) {
+    data = JSON.parse(localData);
+  } else {
+    data = {
+      entries: [],
+      nextEntryId: 1,
+    };
+  }
   return data;
+}
+function writeData(data: Data): void {
+  const dataJSON = JSON.stringify(data);
+  localStorage.setItem(dataKey, dataJSON);
+}
+export function readEntries(): Entry[] {
+  return readData().entries;
+}
+export function addEntry(entry: UnsavedEntry): Entry {
+  const data = readData();
+  const newEntry = {
+    ...entry,
+    entryId: data.nextEntryId++,
+  };
+  data.entries.unshift(newEntry);
+  writeData(data);
+  return newEntry;
+}
+export function updateEntry(entry: Entry): Entry {
+  const data = readData();
+  const newEntries = data.entries.map((e) =>
+    e.entryId === entry.entryId ? entry : e
+  );
+  data.entries = newEntries;
+  writeData(data);
+  return entry;
+}
+export function removeEntry(entryId: number): void {
+  const data = readData();
+  const updatedArray = data.entries.filter(
+    (entry) => entry.entryId !== entryId
+  );
+  data.entries = updatedArray;
+  writeData(data);
 }
